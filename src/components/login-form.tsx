9ae3e2708fe7229203df3921ce9cf8da5500efb3
varun <varun6pain@gmail.com>
2025-02-08 18:56:36 +0530

    updated

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -64,11 +64,19 @@ export function LoginForm({
           <CardDescription>{isLogin ? "Login with your credentials" : "Sign up to get started"}</CardDescription>
         </CardHeader>
         <CardContent>
-          <form onSubmit={handleSubmit}>
-            <div className="grid gap-6">
+            <div className="grid gap-6 mb-6">
               <Button variant="outline" className="w-full" onClick={handleGoogleLogin}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <path
+                      d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                      fill="currentColor"
+                    />
+                  </svg>
                 Login with Google
               </Button>
+              </div>
+              <form onSubmit={handleSubmit}>
+              <div className="grid gap-6">
               <div className="grid gap-2">
                 <Label htmlFor="email">Email</Label>
                 <Input id="email" type="email" value={formData.email} onChange={handleChange} required />
@@ -89,14 +97,16 @@ export function LoginForm({
               <Button type="submit" className="w-full">
                 {isLogin ? "Login" : "Sign up"}
               </Button>
-            </div>
             <div className="text-center text-sm mt-4">
               {isLogin ? "Don't have an account?" : "Already have an account?"} {" "}
               <button type="button" onClick={toggleAuthMode} className="underline underline-offset-4 hover:text-primary">
                 {isLogin ? "Sign up" : "Login"}
               </button>
             </div>
+              </div>
           </form>
+              
+       
         </CardContent>
       </Card>
     </div>
